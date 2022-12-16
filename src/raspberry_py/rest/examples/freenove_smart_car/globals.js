eeaff82494f33f71b991013cb842c404e3973e88
MatthewGerber <gerber.matthew@gmail.com>
2022-12-15 22:07:13 -0500

    First JS code for module import layout.

---
+++ src/raspberry_py/rest/examples/freenove_smart_car/globals.js
@@ -0,0 +1,2 @@
+export const rest_host = "10.0.0.59";
+export const rest_port = 5000;
--- src/raspberry_py/rest/utils.js
+++ src/raspberry_py/rest/utils.js
@@ -1,18 +1,18 @@
 
-element_id_call_time = {};
-element_id_latency = {};
-element_id_alpha = {};
+const element_id_call_time = {};
+const element_id_latency = {};
+const element_id_alpha = {};
 
-function init_latency(element_id, alpha) {
+export function init_latency(element_id, alpha) {
   element_id_latency[element_id] = null;
   element_id_alpha[element_id] = alpha;
 }
 
-function set_call_time(element_id) {
+export function set_call_time(element_id) {
   element_id_call_time[element_id] = Date.now()
 }
 
-function update_latency(element_id) {
+export function update_latency(element_id) {
     const latency = Date.now() - element_id_call_time[element_id];
     const alpha = element_id_alpha[element_id];
     let running_latency = element_id_latency[element_id];
@@ -22,10 +22,11 @@ function update_latency(element_id) {
     else {
       running_latency = alpha * running_latency + (1.0 - alpha) * latency;
     }
+    element_id_latency[element_id] = running_latency;
     return running_latency;
 }
 
-async function is_checked(element) {
+export async function is_checked(element) {
   while (!element.is(":checked")) {
     await new Promise(r => setTimeout(r, 1000));
   }
