0a48bff5ee9c640131fc60538edf2d8772f394cc
Matthew Gerber <matthew.gerber@dexcom.com>
2024-01-02 22:21:19 -0500

    Rotary encoder clean-up.

--- src/raspberry_py/gpio/__init__.py
+++ src/raspberry_py/gpio/__init__.py
@@ -201,7 +201,7 @@ class Component(ABC):
         :param action: Function to run when event is triggered. Accepts the component's current state.
         :param trigger: A function that takes the component state and returns True if action should be triggered, or
         None to trigger the event on every state change.
-        :param synchronous: Whether or not the action function should be called synchronously. If True, then execution
+        :param synchronous: Whether the action function should be called synchronously. If True, then execution
         will not resume until the action function has returned. If False, the action function will be started in a new
         thread and execution will resume immediately.
         """
