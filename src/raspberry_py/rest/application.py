b7c5bffbc1d2bacda6aa1efaefc602943485338e
Matthew Gerber <mvg0419@dexcom.com>
2026-03-28 20:39:25 -0400

    * Return image from center. * Add default blank-image bytes.

--- src/raspberry_py/rest/application.py
+++ src/raspberry_py/rest/application.py
@@ -70,7 +70,7 @@ class Call:
         self.function_name = function_name
         self.arg_value = arg_value
 
-        self.call_image_bytes: Optional[CallImageBytes] = None
+        self.call_image_bytes: CallImageBytes = CallImageBytes('/9j/4AAQSkZJRgABAQEAYABgAAD/2wBDAAgGBgcGBQgHBwcJCQgKDBQNDAsLDBkSEw8UHRofHh0aHBwgJC4nICIsIxwcKDcpLDAxNDQ0Hyc5PTgyPC4zNDL/2wBDAQkJCQwLDBgNDRgyIRwhMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjL/wAARCAABAAEDASIAAhEBAxEB/8QAHwAAAQUBAQEBAQEAAAAAAAAAAAECAwQFBgcICQoL/8QAtRAAAgEDAwIEAwUFBAQAAAF9AQIDAAQRBRIhMUEGE1FhByJxFDKBkaEII0KxwRVS0fAkM2JyggkKFhcYGRolJicoKSo0NTY3ODk6Q0RFRkdISUpTVFVWV1hZWmNkZWZnaGlqc3R1dnd4eXqDhIWGh4iJipKTlJWWl5iZmqKjpKWmp6ipqrKztLW2t7i5usLDxMXGx8jJytLT1NXW19jZ2uHi4+Tl5ufo6erx8vP09fb3+Pn6/8QAHwEAAwEBAQEBAQEBAQAAAAAAAAECAwQFBgcICQoL/8QAtREAAgECBAQDBAcFBAQAAQJ3AAECAxEEBSExBhJBUQdhcRMiMoEIFEKRobHBCSMzUvAVYnLRChYkNOEl8RcYGRomJygpKjU2Nzg5OkNERUZHSElKU1RVVldYWVpjZGVmZ2hpanN0dXZ3eHl6goOEhYaHiImKkpOUlZaXmJmaoqOkpaanqKmqsrO0tba3uLm6wsPExcbHyMnK0tPU1dbX2Nna4uPk5ebn6Onq8vP09fb3+Pn6/9oADAMBAAIRAxEAPwD3+iiigD//2Q==')
 
     def __eq__(
             self,
@@ -244,7 +244,7 @@ class CallHistory(Component):
 
         return [
             {
-                'image': f'{"" if c.call_image_bytes is None else c.call_image_bytes}',
+                'image': f'{c.call_image_bytes}',
                 'title': f'{c.function_name}',
                 'description': f'{c.arg_value}'
             }
