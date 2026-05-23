b0862d66eaa817330fde23cd6306f8fcde5c119f
MatthewGerber <gerber.matthew@gmail.com>
2026-05-23 15:51:15 -0400

    RPI5 compatible version.

---
+++ src/raspberry_py/gpio/examples/led_strip_adafruit_rpi5.py
@@ -0,0 +1,46 @@
+import time
+
+import adafruit_pixelbuf
+import board
+from adafruit_led_animation.animation.rainbow import Rainbow
+from adafruit_led_animation.animation.rainbowchase import RainbowChase
+from adafruit_led_animation.animation.rainbowcomet import RainbowComet
+from adafruit_led_animation.animation.rainbowsparkle import RainbowSparkle
+from adafruit_led_animation.sequence import AnimationSequence
+from adafruit_raspberry_pi5_neopixel_write import neopixel_write
+
+NEOPIXEL = board.MOSI
+num_pixels = 144
+
+class Pi5Pixelbuf(adafruit_pixelbuf.PixelBuf):
+    def __init__(self, pin, size, **kwargs):
+        self._pin = pin
+        super().__init__(size=size, **kwargs)
+
+    def _transmit(self, buf):
+        neopixel_write(self._pin, buf)
+
+pixels = Pi5Pixelbuf(NEOPIXEL, num_pixels, auto_write=True, byteorder="BGR")
+
+rainbow = Rainbow(pixels, speed=0.02, period=2)
+rainbow_chase = RainbowChase(pixels, speed=0.02, size=5, spacing=3)
+rainbow_comet = RainbowComet(pixels, speed=0.02, tail_length=7, bounce=True)
+rainbow_sparkle = RainbowSparkle(pixels, speed=0.02, num_sparkles=15)
+
+
+animations = AnimationSequence(
+    rainbow,
+    rainbow_chase,
+    rainbow_comet,
+    rainbow_sparkle,
+    advance_interval=5,
+    auto_clear=True,
+)
+
+try:
+    while True:
+        animations.animate()
+finally:
+    time.sleep(.02)
+    pixels.fill(0)
+    pixels.show()
