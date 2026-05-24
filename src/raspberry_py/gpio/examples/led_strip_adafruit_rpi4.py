84719cd8dbc1da5721a3c8537328d57cef50b875
Matthew Gerber <mvg0419@dexcom.com>
2026-05-24 00:08:18 -0400

    Clean up.

--- src/raspberry_py/gpio/examples/led_strip_adafruit_rpi4.py
+++ src/raspberry_py/gpio/examples/led_strip_adafruit_rpi4.py
@@ -6,7 +6,17 @@ from raspberry_py.gpio import setup
 from raspberry_py.gpio.examples.led_strip import run
 from raspberry_py.gpio.lights import LedStrip
 
-setup(gpio.BCM)
-pixels = neopixel.NeoPixel(board.MOSI, 144, brightness=0.1, auto_write=False)
-led_strip = LedStrip(pixels)
-run(led_strip)
+
+def main():
+    """
+    Run an LED strip test for Raspberry Pi 4.
+    """
+
+    setup(gpio.BCM)
+    pixels = neopixel.NeoPixel(board.MOSI, 144, brightness=0.1, auto_write=False)
+    led_strip = LedStrip(pixels)
+    run(led_strip)
+
+
+if __name__ == '__main__':
+    main()
--- src/raspberry_py/gpio/examples/led_strip_adafruit_rpi5.py
+++ src/raspberry_py/gpio/examples/led_strip_adafruit_rpi5.py
@@ -1,11 +1,21 @@
+import RPi.GPIO as gpio
 import board
 
-import RPi.GPIO as gpio
 from raspberry_py.gpio import setup
 from raspberry_py.gpio.examples.led_strip import run
 from raspberry_py.gpio.lights import Pi5PixelBuffer, LedStrip
 
-setup(gpio.BCM)
-pixels = Pi5PixelBuffer(board.MOSI, 144, auto_write=True, byteorder="BGR")
-led_strip = LedStrip(pixels)
-run(led_strip)
+
+def main():
+    """
+    Run an LED strip test for Raspberry Pi 5.
+    """
+
+    setup(gpio.BCM)
+    pixels = Pi5PixelBuffer(board.MOSI, 144, auto_write=True, byteorder="BGR")
+    led_strip = LedStrip(pixels)
+    run(led_strip)
+
+
+if __name__ == '__main__':
+    main()
